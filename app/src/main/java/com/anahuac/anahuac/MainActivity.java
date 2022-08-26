ecda647b3ba178d6d75920798842f16d1bfcf616
brendamedina <bren1096@hotmail.com>
2022-08-25 21:38:09 -0500

    login netflix

--- app/src/main/java/com/anahuac/anahuac/MainActivity.java
+++ app/src/main/java/com/anahuac/anahuac/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity{
 
     private Button btnLoginFacebook;
+    private Button getBtnSignInNetflix;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         btnLoginFacebook = findViewById(R.id.btn_login_facebook);
+        getBtnSignInNetflix = findViewById(R.id.btn_sign_in_netflix);
 
 
         btnLoginFacebook.setOnClickListener(new View.OnClickListener() {
@@ -30,7 +33,18 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+        getBtnSignInNetflix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Boton pulsado", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, LoginNetflixActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 
 
         /*if(view.getId() == R.id.btn_accept){
